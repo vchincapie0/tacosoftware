075b03aa939803926480018cc1c03b7bbc4696ec
vivian_pc <carohinca1997@gmail.com>
2024-06-01 18:18:41 -0500

    Se crea url para ExistenciasProductoTerminadoListView

--- sistema/applications/existencias/urls.py
+++ sistema/applications/existencias/urls.py
@@ -6,4 +6,6 @@ app_name='existencias_app'
 urlpatterns = [
     path('existencias_mp/',views.ExistenciasMateriaPrimaListView.as_view(),name='stock_mp'),
     path('existencias_it/',views.ExistenciasInsumosListView.as_view(),name='stock_it'),
+    path('existencias_pt/',views.ExistenciasProductoTerminadoListView.as_view(),name='stock_pt'),
+
 ]
