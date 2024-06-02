c7bacf8fd1dd111b2ef353d671681e2d3deac070
vivian_pc <carohinca1997@gmail.com>
2024-06-02 14:20:05 -0500

    Se comenta managers users

--- sistema/applications/users/managers.py
+++ sistema/applications/users/managers.py
@@ -1,27 +1,36 @@
+# Fecha de Creación: 02/02/2024
+# Autor: Vivian Carolina Hincapie Escobar
+# Última modificación: 16/02/2024
+
 from django.db import models
 from django.contrib.auth.models import BaseUserManager
 
+# Clase UserManager: un administrador de usuarios personalizado que extiende BaseUserManager y models.Manager
 class UserManager(BaseUserManager, models.Manager):
 
-    def _create_user(self, name, last_name,username, password, is_staff, is_superuser, **extra_fields):
+    # Método para crear un usuario básico
+    def _create_user(self, name, last_name, username, password, is_staff, is_superuser, **extra_fields):
+        # Crea una nueva instancia de usuario con los campos proporcionados
         user = self.model(
             name=name,
             last_name=last_name,
-            username = username,
+            username=username,
             is_staff=is_staff,
             is_superuser=is_superuser,
             **extra_fields
         )
 
+        # Establece la contraseña del usuario y guarda el usuario en la base de datos
         user.set_password(password)
         user.save(using=self.db)
         return user
     
-    def create_user(self, name, last_name,username,password=None,**extrafields):
-        return self._create_user(name,last_name,username, password,False,False, **extrafields)
+    # Método para crear un usuario normal
+    def create_user(self, name, last_name, username, password=None, **extra_fields):
+        # Llama a _create_user con is_staff y is_superuser establecidos en False
+        return self._create_user(name, last_name, username, password, False, False, **extra_fields)
 
-    def create_superuser(self, name,last_name,username, password=None, **extra_fields):
-        return self._create_user(name,last_name,username,password, True, True,**extra_fields)
-
-
-
+    # Método para crear un superusuario
+    def create_superuser(self, name, last_name, username, password=None, **extra_fields):
+        # Llama a _create_user con is_staff y is_superuser establecidos en True
+        return self._create_user(name, last_name, username, password, True, True, **extra_fields)
