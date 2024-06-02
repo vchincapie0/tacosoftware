8c4b1bd786a18865322867d566f6929b3dfc447c
vivian_pc <carohinca1997@gmail.com>
2024-06-02 14:30:40 -0500

    Se comenta admin de usuarios

--- sistema/applications/users/admin.py
+++ sistema/applications/users/admin.py
@@ -1,8 +1,11 @@
+# Fecha de Creación: 02/02/2024
+# Autor: Vivian Carolina Hincapie Escobar
+# Última modificación: 23/04/2024
+
 from django.contrib import admin
 from . models import User, UserAudit
 
-# Register your models here.
-
+#Registrar los modelos aquí
 admin.site.register(User)
 admin.site.register(UserAudit)
 
