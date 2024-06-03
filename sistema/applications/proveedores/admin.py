b2866b1aa4986b9f8afe27b0f06453b83e2c4345
vivian_pc <carohinca1997@gmail.com>
2024-06-03 13:09:31 -0500

    Se comenta admin proveedores

--- sistema/applications/proveedores/admin.py
+++ sistema/applications/proveedores/admin.py
@@ -1,7 +1,12 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
+# Fecha de Creación: 02/02/2024
+# Autor: Vivian Carolina Hincapie Escobar
+# Última modificación: 29/04/2024
 
+from django.contrib import admin
+from .models import Proveedores, ProveedoresAudit
+
+# Registro de los modelos en el sitio de administración de Django.
+# Esto permite gestionar los modelos Proveedores y ProveedoresAudit a través del panel de administración.
 
 admin.site.register(Proveedores)
 admin.site.register(ProveedoresAudit)
