9969c3b15a5653866e46422ac7edd6cef597a398
vivian_pc <carohinca1997@gmail.com>
2024-06-03 12:56:51 -0500

    Se comenta codigo url de proveedores

--- sistema/applications/proveedores/urls.py
+++ sistema/applications/proveedores/urls.py
@@ -1,3 +1,7 @@
+# Fecha de Creación: 16/02/2024
+# Autor: Vivian Carolina Hincapie Escobar
+# Última modificación: 16/05/2024
+
 from django.urls import path
 from applications.users.decorators import admin_required
 from . import views
@@ -7,7 +11,7 @@ app_name='proveedores_app'
 urlpatterns = [
     path('list_proveedores/',admin_required(views.ProveedoresListView.as_view()),name='list_proveedores'),
     path('proveedores/export/xls',admin_required(views.export_proveedores_to_excel), name='export_proveedores_excel'),
-    path('proveedores/export/cvs',admin_required(views.export_proveedores_to_csv), name='export_proveedores_cvs'),
+    path('proveedores/export/csv',admin_required(views.export_proveedores_to_csv), name='export_proveedores_cvs'),
     path('add_proveedor/',admin_required(views.ProveedoresCreateView.as_view()),name='add_proveedores'),
     path('edit_proveedor/<pk>',admin_required(views.ProveedorUpdateView.as_view()),name='update_proveedores'),
     path('delete_proveedor/<pk>',admin_required(views.ProveedoresDeleteView.as_view()),name='delete_proveedores'),
