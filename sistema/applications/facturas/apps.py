ff70a973bef653881184741851da88935a26abf3
vivian_pc <carohinca1997@gmail.com>
2024-06-03 18:36:05 -0500

    Se comenta apps de facturas

--- sistema/applications/facturas/apps.py
+++ sistema/applications/facturas/apps.py
@@ -1,5 +1,8 @@
-from django.apps import AppConfig
+# Fecha de Creación: 27/02/2024
+# Autor: Vivian Carolina Hincapie Escobar
+# Última modificación: 11/04/2024
 
+from django.apps import AppConfig
 
 class FacturasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
