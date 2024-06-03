371ca43bd3a13cccfb8f3db16015b9a95dc09fcf
vivian_pc <carohinca1997@gmail.com>
2024-06-03 18:57:50 -0500

    Se comenta apps de existencias

--- sistema/applications/existencias/apps.py
+++ sistema/applications/existencias/apps.py
@@ -1,5 +1,8 @@
-from django.apps import AppConfig
+# Fecha de Creación: 14/04/2024
+# Autor: Vivian Carolina Hincapie Escobar 
+# Última modificación: 21/05/2024
 
+from django.apps import AppConfig
 
 class ExistenciasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
