017c6347892a9de03aad67d6e574d455db235462
vivian_pc <carohinca1997@gmail.com>
2024-06-04 16:49:14 -0500

    Se comenta signals de pedidos

--- sistema/applications/pedidos/signals.py
+++ sistema/applications/pedidos/signals.py
@@ -1,21 +1,26 @@
+# Fecha de Creación: 18/03/2024
+# Autor: Vivian Carolina Hincapie Escobar
+# Última modificación: 29/04/2024
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from .models import Pedidos, PedidosAudit
 import threading
 
-
-
 # Obtener el modelo de usuario personalizado
 User = get_user_model()
 
 @receiver(post_save, sender=Pedidos)
 def log_pedidos_change(sender, instance, created, **kwargs):
+    '''Definición de la función que maneja la señal después de guardar un objeto Pedidos'''
     current_user = getattr(threading, 'current_user', None)
 
+    # Obtener el usuario actual que realizó la acción
     if current_user:
         changed_by = current_user
 
+    # Determinar la acción realizada y los detalles para el registro de auditoría
     if instance.deleted:
         action = 'D'  # Marcar como eliminado
         details = f"{instance.ref_pedido} ha sido borrado."
